f17eedd7d232aaa987e5f9b3bc616ce3607329c2
David Shaw <david.shaw1242@gmail.com>
2025-01-01 09:12:32 -0500

    working on getting the internal config package working with the main func and then printing the home variable to figure how that works

--- internal/config/Read.go
+++ internal/config/Read.go
@@ -1,9 +1,18 @@
 package config
 
 import (
+	"fmt"
 	"os"
 )
 
-func Read() Config {
+func Read() (Config, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("an error occurred getting the home dir: ")
+		return Config{}, err
+	}
+	fmt.Println(homeDir)
+
+	return Config{}, nil
 
 }
