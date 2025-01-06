7a6b1c8197292fe69c0d7b59593ae40c64d5e312
David Shaw <david.shaw1242@gmail.com>
2025-01-06 10:14:17 -0500

    added some output formatting

--- handler_feeds.go
+++ handler_feeds.go
@@ -11,8 +11,17 @@ func handlerFeeds(s *state, cmd command) error {
 		return err
 	}
 
-	for i := 0; i < len(feeds); i++ {
-		fmt.Println(feeds[i])
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found.")
+		return nil
+	}
+	fmt.Println("Feeds Found")
+	fmt.Println("==============================")
+	for _, feed := range feeds {
+		fmt.Printf("Feed Name:	       %s\n", feed.Name)
+		fmt.Printf("Feed URL:            %s\n", feed.Url)
+		fmt.Printf("User Name:	       %s\n", feed.Name_2)
+		fmt.Println("==============================")
 	}
 
 	return nil
