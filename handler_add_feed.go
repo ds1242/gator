25fbe8d0d7a06adf4234a79385c072a89f11ec56
David Shaw <david.shaw1242@gmail.com>
2025-01-06 09:30:41 -0500

    started add feed handler, created a struct and a func to handle printing the returned values when successfully adding a new feed

---
+++ handler_add_feed.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ds1242/gator.git/internal/database"
+	"github.com/google/uuid"
+)
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	URL       string    `json:"url"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func databaseFeedToFeed(feed database.Feed) Feed {
+	return Feed{
+		ID:        feed.ID,
+		CreatedAt: feed.CreatedAt,
+		UpdatedAt: feed.UpdatedAt,
+		Name:      feed.Name,
+		URL:       feed.Url,
+		UserID:    feed.UserID,
+	}
+}
+
+func handlerAddFeed(s *state, cmd command, name string, url string) error {
+	currentUserName := s.config.CurrentUserName
+	currentUserId, err := s.db.GetUser(context.Background(), currentUserName)
+	if err != nil {
+		return err
+	}
+	feed, err := s.db.AddToFeed(context.Background(), database.AddToFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+		Url:       url,
+		UserID:    currentUserId.ID,
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(databaseFeedToFeed(feed))
+	return nil
+
+}
