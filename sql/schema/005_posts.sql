73202205e241b7c1c9e0d33a615f16e4db18c54a
David Shaw <david.shaw1242@gmail.com>
2025-01-08 10:33:48 -0500

    added a new posts to the db

--- sql/schema/005_posts.sql
+++ sql/schema/005_posts.sql
@@ -7,7 +7,7 @@ CREATE TABLE posts (
 	url TEXT UNIQUE NOT NULL,
 	description TEXT,
 	published_at TIMESTAMP,
-	feed_id UUID
+	feed_id UUID,
 	CONSTRAINT fk_feed_id
 		FOREIGN KEY(feed_id)
 		REFERENCES feeds(id)
