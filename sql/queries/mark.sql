c9584987d9270fc132319352559ec4832a5b4bac
David Shaw <david.shaw1242@gmail.com>
2025-01-08 09:17:47 -0500

    setup select next feed to fetch query, ran sqlc generate to generate go code for both new queries

--- sql/queries/mark.sql
+++ sql/queries/mark.sql
@@ -1,6 +1,11 @@
 -- name: MarkFeedFetched :one
 UPDATE feeds
 SET updated_at = $1, last_fetched_at = $2
-WHERE id = $3;
+WHERE id = $3
+RETURNING *;
 
--- name: GetNextFeedToFetch
+-- name: GetNextFeedToFetch :one
+SELECT id, url
+FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
